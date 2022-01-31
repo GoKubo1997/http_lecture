efb87eb9a596faf4c51f34f6c9683f191f0d2d08
GOKUBO1997 <GOKUBOCONTACT@GMAIL.COM>
2022-01-31 23:08:05 +0900

    k

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 // view models
 import 'view_models/image_list_cubit.dart';
